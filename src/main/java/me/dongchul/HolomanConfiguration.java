4b33783f295d618043027da25ea0916758787e8a
dongchul <sah3122@naver.com>
2019-11-10 15:22:49 +0900

    ADD AutoConfiguration properties & Conditaional Bean

--- src/main/java/me/dongchul/HolomanConfiguration.java
+++ src/main/java/me/dongchul/HolomanConfiguration.java
@@ -1,16 +1,20 @@
 package me.dongchul;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
 public class HolomanConfiguration {
 
     @Bean
-    public Holoman holoman() {
+    @ConditionalOnMissingBean // 덮어 쓰기 방지.
+    public Holoman holoman(HolomanProperties properties) {
         Holoman holoman = new Holoman();
-        holoman.setHowLong(5);
-        holoman.setName("dongchul");
+        holoman.setHowLong(properties.getHowLong());
+        holoman.setName(properties.getName());
         return holoman;
     }
 }
